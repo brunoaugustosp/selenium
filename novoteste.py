f95d63cdc3a97074f7fb18bdfc347f648cb63b14
Bruno <brunoaugustosp@outlook.com.br>
2021-06-01 00:01:13 -0300

    teste

---
+++ novoteste.py
@@ -0,0 +1,16 @@
+
+# i = 5
+# for i in range(25):
+   
+#     print(i)
+
+
+
+
+
+row = 1
+while row <= 25:
+
+    print(row)
+    row += 1
+
--- text.py
+++ text.py
@@ -5,28 +5,35 @@ import os
 from time import sleep
 
 
-url = 'https://finance.yahoo.com/screener/new'
+
 
 driver = webdriver.Chrome(executable_path=r'./chromedriver.exe')
-driver.get(url)
+driver.get('https://finance.yahoo.com/screener/new')
 
 
 
 element01 = driver.find_element_by_xpath('//*[@id="screener-criteria"]/div[2]/div[1]/div[1]/div[1]/div/div[2]/ul/li[1]/button').click()
 element02 = driver.find_element_by_xpath('//*[@id="screener-criteria"]/div[2]/div[1]/div[1]/div[1]/div/div[2]/ul/li/div/div').click()
-element02 = driver.find_element_by_xpath('//*[@id="screener-criteria"]/div[2]/div[1]/div[1]/div[1]/div/div[2]/ul/li/div/div').click()
-sleep(1)
-find = driver.find_elements_by_xpath("//*[label[text()='Argentina']]/input").click()
+find = driver.find_element_by_xpath('//span[text()="Argentina"]').click()
+sleep(2)
+element02 = driver.find_element_by_xpath('//*[@id="screener-criteria"]/div[2]/div[1]/div[3]/button[1]').click()
+sleep(2)
 
 
-sleep(25)
 
-estimated_result = driver.find_element_by_xpath('//*[@id="screener-criteria"]/div[2]/div[1]/div[2]/div/div[2]/div').text
+row = 1
+while row <= 25:
 
-symbol = driver.find_element_by_xpath('//*[@id="scr-res-table"]/div[1]/table/tbody/tr[1]/td[1]/a').text
-name = driver.find_element_by_xpath('//*[@id="scr-res-table"]/div[1]/table/tbody/tr[1]/td[2]').text
-price = driver.find_element_by_xpath('//*[@id="scr-res-table"]/div[1]/table/tbody/tr[1]/td[3]/span').text
+    estimated_result = driver.find_element_by_xpath('//*[@id="screener-criteria"]/div[2]/div[1]/div[2]/div/div[2]/div').text
+    symbol = driver.find_element_by_xpath(f'//*[@id="scr-res-table"]/div[1]/table/tbody/tr[{row}]/td[1]/a').text
+    name = driver.find_element_by_xpath(f'//*[@id="scr-res-table"]/div[1]/table/tbody/tr[{row}]/td[2]').text
+    price = driver.find_element_by_xpath(f'//*[@id="scr-res-table"]/div[1]/table/tbody/tr[{row}]/td[3]/span').text
 
+    print(estimated_result)
+    print(symbol)
+    print(name)
+    print(price)
+    row += 1
 
 
 
